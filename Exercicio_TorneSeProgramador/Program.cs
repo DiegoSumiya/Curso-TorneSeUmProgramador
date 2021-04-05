c86805c5a76ba5336f652bd60a047156b525b069
DiegoSumiya <diego.sumiya@gmail.com>
2021-04-05 20:05:23 -0300

    Projeto Iniciado

---
+++ Exercicio_TorneSeProgramador/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio_TorneSeProgramador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o número desejado para a tabuáda: ");
+            int numero = int.Parse(Console.ReadLine());
+            CalculoTabuada(numero);
+        }
+
+        public static void CalculoTabuada(int numero)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(numero + " X " + i + " = " + (numero * i));
+            }
+        }
+
+        public static void CalculoMedia(double notas)
+        {
+
+        }
+    }
+}
